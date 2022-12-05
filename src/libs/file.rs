2c307759a82782c50b9ac956608bab4255074528
backend-junsung <leejs1030@cashwalk.io>
2022-12-05 20:45:19 +0900

    add: 폴더/파일 삭제 분리

--- src/libs/file.rs
+++ src/libs/file.rs
@@ -15,21 +15,29 @@ pub async fn read_file(file_name: &str) -> Result<Bytes, (StatusCode, String)> {
     match file {
         Ok(contents) => Ok(Bytes::from(contents)),
         Err(e) => match e.kind() {
-            std::io::ErrorKind::NotFound => Err((
-                StatusCode::NOT_FOUND,
-                String::from("File does not exist!"),
-            )),
-            _ => Err((StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))
+            std::io::ErrorKind::NotFound => {
+                Err((StatusCode::NOT_FOUND, String::from("File does not exist!")))
+            }
+            _ => Err((StatusCode::INTERNAL_SERVER_ERROR, e.to_string())),
         },
     }
 }
 
 pub async fn write_file(file_name: &str, content: Bytes) -> tokio::io::Result<()> {
     let path = "./files".to_owned() + file_name;
+    let last = path.rfind("/").unwrap();
+
+    let true_path = &path[..last];
+    tokio::fs::create_dir_all(true_path).await.unwrap();
     tokio::fs::write(path, content).await
 }
 
 pub async fn remove_file(file_name: &str) -> tokio::io::Result<()> {
     let path = "./files".to_owned() + file_name;
     tokio::fs::remove_file(path).await
+}
+
+pub async fn remove_dir(file_name: &str) -> tokio::io::Result<()> {
+    let path = "./files".to_owned() + file_name;
+    tokio::fs::remove_dir_all(path).await
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 mod libs;
 
-use crate::libs::file::{get_ext_name, read_file, remove_file, write_file};
-use crate::libs::http::{build_json_message_response, build_method_not_found_error_response, file_response, get_json_mime_type, get_mime_type, get_plain_mime_type, set_content_type, set_json_body, set_status_code};
+use crate::libs::file::{get_ext_name, read_file, remove_dir, remove_file, write_file};
+use crate::libs::http::{
+    build_json_message_response, build_method_not_found_error_response, file_response,
+    get_json_mime_type, get_mime_type, get_plain_mime_type, set_content_type, set_json_body,
+    set_status_code,
+};
 use bytes::{BufMut, Bytes, BytesMut};
 use http_body_util::StreamBody;
 use hyper::body::HttpBody;
@@ -26,7 +30,7 @@ async fn get_file(path: &str) -> Response<Body> {
             let mime_type = get_mime_type(ext_name);
             set_content_type(response, mime_type)
         }
-        Err((code, message)) => build_json_message_response(code, &message)
+        Err((code, message)) => build_json_message_response(code, &message),
     }
 }
 
@@ -38,15 +42,59 @@ async fn put_file(file_name: &str, body: Body) -> Response<Body> {
     let res = write_file(file_name, content).await;
     match res {
         Ok(_) => build_json_message_response(StatusCode::OK, "File is successfully created!"),
-        Err(e) => build_json_message_response(StatusCode::INTERNAL_SERVER_ERROR, &e.to_string())
+        Err(e) => build_json_message_response(StatusCode::INTERNAL_SERVER_ERROR, &e.to_string()),
     }
 }
 
-async fn delete_file(path: &str) -> Response<Body> {
-    let res = remove_file(path).await;
+async fn delete_file(path: &str, query: &str) -> Response<Body> {
+    let params = query.split("&");
+    let mut is_directory: Option<bool> = None;
+    for param in params {
+        let mut itr = param.split("=");
+        let key = itr.next();
+        let value = itr.next();
+        let extra = itr.next();
+        if extra != None {
+            return build_json_message_response(StatusCode::BAD_REQUEST, "Invalid query params");
+        }
+        if value == None {
+            return build_json_message_response(StatusCode::BAD_REQUEST, "Invalid query params");
+        }
+        if key == None {
+            return build_json_message_response(StatusCode::BAD_REQUEST, "Invalid query params");
+        }
+        if key.unwrap() == "type" {
+            if is_directory != None {
+                return build_json_message_response(
+                    StatusCode::BAD_REQUEST,
+                    "Invalid query params",
+                );
+            }
+            let value = value.unwrap();
+            if value == "file" {
+                is_directory = Some(false);
+            } else if value == "directory" {
+                is_directory = Some(true);
+            } else {
+                return build_json_message_response(
+                    StatusCode::BAD_REQUEST,
+                    "Invalid query params",
+                );
+            }
+        }
+    }
+
+    let is_directory = match is_directory{
+        Some(t) => t,
+        None => false
+    };
+    let res = match is_directory {
+        false => remove_file(path).await,
+        true => remove_dir(path).await,
+    };
     match res {
         Ok(_) => build_json_message_response(StatusCode::OK, "File is successfully deleted!"),
-        Err(e) => build_json_message_response(StatusCode::INTERNAL_SERVER_ERROR, &e.to_string())
+        Err(e) => build_json_message_response(StatusCode::INTERNAL_SERVER_ERROR, &e.to_string()),
     }
 }
 
@@ -54,12 +102,13 @@ async fn top_router(req: Request<Body>) -> Result<Response<Body>, Infallible> {
     let t = tokio::spawn(async move {
         let (parts, body) = req.into_parts();
         let path = parts.uri.path();
+        let query = parts.uri.query().unwrap_or("");
         let method = parts.method;
 
         let response = match method {
             Method::GET => get_file(path).await,
             Method::PUT => put_file(path, body).await,
-            Method::DELETE => delete_file(path).await,
+            Method::DELETE => delete_file(path, query).await,
             _ => build_method_not_found_error_response(),
         };
         println!("{:?}", response);
